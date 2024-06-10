ed85fb1e17a4da38e859f5a980e0f0096a954c21
Joseph Castillo <joseph.cast0226@gmail.com>
2024-06-10 16:04:51 -0700

    Add placeholder icons to HTML skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -10,15 +10,36 @@ function App() {
           <li>now</li>
           <li>projects</li>
           <li>resume</li>
+          <li>
+          <span class="material-symbols-outlined"> light_mode </span>
+          </li>
         </ul>
       </div>
-      <h1>Hi! I'm Joseph.</h1>
-      <p>
-        This is where I will fill in my personal description.
-        Right now, I'm just focusing on getting the structure in place first.
-      </p>
-      <h2>Contact me!</h2>
-      <p>Designed & Built by Joseph Castillo</p>
+      <div class="main">
+        <div class="text">
+          <h1>Hi! I'm Joseph.</h1>
+          <p>
+            This is where I will fill in my personal description.
+            Right now, I'm just focusing on getting the structure in place first.
+          </p>
+        </div>
+        <img class="headshot" src="src/assets/headshot_placeholder.jpg" alt="Personal site headshot placeholder"></img>
+      </div>
+      <div class="contact">
+        <h2 class="contact">Contact me!</h2>
+        <ul>
+          <li>
+            <img class="contact-icon" src="src/assets/icon_placeholder.jpg" alt="Placeholder Icon"></img>
+          </li>
+          <li>
+            <img class="contact-icon" src="src/assets/icon_placeholder.jpg" alt="Placeholder Icon"></img>
+          </li>
+          <li>
+            <img class="contact-icon" src="src/assets/icon_placeholder.jpg" alt="Placeholder Icon"></img>
+          </li>
+        </ul>
+      </div>
+      <p class="footer">Designed & Built by Joseph Castillo</p>
     </>
   )
 }
