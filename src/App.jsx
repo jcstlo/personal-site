c505fddaa96532f431bf5a6e82ed7d53f860beb6
Joseph Castillo <joseph.cast0226@gmail.com>
2024-06-10 14:22:49 -0700

    Remove unnecessary Vite template code

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <h1>Hi! I'm Joseph.</h1>
+      <p>
+        This is where I will fill in my personal description.
+        Right now, I'm just focusing on getting the structure in place first.
       </p>
+      <h2>Contact me!</h2>
+      <p>Designed & Built by Joseph Castillo</p>
     </>
   )
 }
