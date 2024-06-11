2028a5c94b675c9e296789e80970a885e867c96e
Joseph Castillo <joseph.cast0226@gmail.com>
2024-06-11 16:21:17 -0700

    Replace existing vanilla CSS with tailwind classes

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,43 @@
-import './App.css'
-
 function App() {
   return (
     <>
-      <div class="header">
-        <p class="text-3xl font-bold underline">Joseph Castillo</p>
+      <div class="header flex justify-between mt-5">
+        <p class="p-0 list-none inline ml-5">Joseph Castillo</p>
         <ul>
-          <li>about</li>
-          <li>now</li>
-          <li>projects</li>
-          <li>resume</li>
-          <li>
+          <li class="p-0 list-none inline mr-10">about</li>
+          <li class="p-0 list-none inline mr-10">now</li>
+          <li class="p-0 list-none inline mr-10">projects</li>
+          <li class="p-0 list-none inline mr-10">resume</li>
+          <li class="p-0 list-none inline mr-10">
           <span class="material-symbols-outlined"> light_mode </span>
           </li>
         </ul>
       </div>
-      <div class="main">
-        <div class="text">
+      <div class="main flex justify-between mt-24 mb-36">
+        <div class="text ml-52 mr-12">
           <h1>Hi! I'm Joseph.</h1>
           <p>
             This is where I will fill in my personal description.
             Right now, I'm just focusing on getting the structure in place first.
           </p>
         </div>
-        <img class="headshot" src="src/assets/headshot_placeholder.jpg" alt="Personal site headshot placeholder"></img>
+        <img class="w-60 h-52 mr-52" src="src/assets/headshot_placeholder.jpg" alt="Personal site headshot placeholder"></img>
       </div>
-      <div class="contact">
+      <div class="contact text-center">
         <h2 class="contact">Contact me!</h2>
-        <ul>
-          <li>
-            <img class="contact-icon" src="src/assets/icon_placeholder.jpg" alt="Placeholder Icon"></img>
+        <ul class="p-0 flex justify-center">
+          <li class="p-0 list-none inline">
+            <img class="contact-icon w-12 h-12" src="src/assets/icon_placeholder.jpg" alt="Placeholder Icon"></img>
           </li>
-          <li>
-            <img class="contact-icon" src="src/assets/icon_placeholder.jpg" alt="Placeholder Icon"></img>
+          <li class="p-0 list-none inline">
+            <img class="contact-icon w-12 h-12" src="src/assets/icon_placeholder.jpg" alt="Placeholder Icon"></img>
           </li>
-          <li>
-            <img class="contact-icon" src="src/assets/icon_placeholder.jpg" alt="Placeholder Icon"></img>
+          <li class="p-0 list-none inline">
+            <img class="contact-icon w-12 h-12" src="src/assets/icon_placeholder.jpg" alt="Placeholder Icon"></img>
           </li>
         </ul>
       </div>
-      <p class="footer">Designed & Built by Joseph Castillo</p>
+      <p class="footer text-center">Designed & Built by Joseph Castillo</p>
     </>
   )
 }
