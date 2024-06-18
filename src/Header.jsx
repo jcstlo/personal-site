d16fb7e68c8d403ab3f9a7c253d2b33e1be2a248
Joseph Castillo <joseph.cast0226@gmail.com>
2024-06-18 11:13:19 -0700

    Make header links (index, now) clickable

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,11 +1,17 @@
+import { Link } from "react-router-dom";
+
 function Header() {
   return (
     <div class="flex justify-between mt-5">
-      <p class="p-0 ml-5 text-2xl inline-block align-text-bottom font-albert font-bold leading-none">Joseph Castillo</p>
+      <div class="p-0 ml-5 text-2xl inline-block align-text-bottom font-albert font-bold leading-none">
+        <Link to={`/`} >Joseph Castillo</Link>
+      </div>
       <div class="flex">
         <ul>
           <li class="p-0 list-none inline mr-10 font-albert font-bold">about</li>
-          <li class="p-0 list-none inline mr-10 font-albert font-bold">now</li>
+          <li class="p-0 list-none inline mr-10 font-albert font-bold">
+            <Link to={`now`}>now</Link>
+          </li>
           <li class="p-0 list-none inline mr-10 font-albert font-bold">projects</li>
           <li class="p-0 list-none inline mr-10 font-albert font-bold">resume</li>
         </ul>
