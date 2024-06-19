d41b1a995136a79d84dd0ab58b7e3ffe47e7e273
Joseph Castillo <joseph.cast0226@gmail.com>
2024-06-18 20:32:38 -0700

    Fix issue where visible scrollbar shifts centered content

--- src/About.jsx
+++ src/About.jsx
@@ -5,7 +5,7 @@ import Footer from "./Footer"
 function About() {
   return (
     <>
-      <div class="mt-24 mb-10 w-screen max-w-5xl">
+      <div class="mt-24 mb-10 max-w-5xl">
         <h1 class="text-5xl mb-7 font-albert font-bold">About Me</h1>
         <p class="font-albert font-normal text-xl">
           I am an aspiring software engineer located in Vancouver, BC. I'm a career transitioner from the semiconductor space. I am passionate about A, B, and C.<br/><br/>
