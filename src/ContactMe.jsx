2c5f3b594f5da457f593ef945d8f043f3152e124
Joseph Castillo <joseph.cast0226@gmail.com>
2024-06-21 10:28:04 -0700

    Turn Github/LinkedIn icons into clickable links

--- src/ContactMe.jsx
+++ src/ContactMe.jsx
@@ -3,8 +3,12 @@ function ContactMe() {
     <div class="text-center">
       <h2 class="text-4xl pb-3 font-albert font-bold">Contact me!</h2>
       <div class="p-0 flex justify-center items-center">
-        <img class="contact-icon w-9 h-9" src="github-logo.svg" alt="Placeholder Icon"></img>
-        <img class="contact-icon w-12 h-12" src="linkedin-logo.svg" alt="Placeholder Icon"></img>
+        <a target="_blank" rel="noopener noreferrer" href="https://github.com/jcstlo">
+          <img class="contact-icon w-9 h-9" src="github-logo.svg" alt="Placeholder Icon"></img>
+        </a>
+        <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/josephcast/">
+          <img class="contact-icon w-12 h-12" src="linkedin-logo.svg" alt="Placeholder Icon"></img>
+        </a>
       </div>
       <p class="mt-3 font-albert underline">joseph.cast0226@gmail.com</p>
     </div>
