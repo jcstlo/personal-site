4a39f0d9fcd88ccd5738b431644f3882ec1ff8bc
Joseph Castillo <joseph.cast0226@gmail.com>
2024-06-22 21:37:23 -0700

    Add repo link (personal-site) to footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,6 +1,21 @@
+import { useContext } from "react";
+import { DarkModeContext } from "./DarkModeContext";
+
 function Footer() {
+  const {darkMode, setDarkMode} = useContext(DarkModeContext);
+  
+  let githubIcon;
+  if (darkMode) {
+    githubIcon = <img class="contact-icon w-5 h-5 ml-2" src="github-logo-dark-mode.svg"></img>;
+  } else {
+    githubIcon = <img class="contact-icon w-5 h-5 ml-2" src="github-logo-light-mode.svg"></img>;
+  }
+  
   return (
-    <p class="text-center pt-10 font-albert font-light text-lightModeText dark:text-darkModeText">Designed & Built by Joseph Castillo</p>
+    <div class="flex mt-10 items-center justify-center">
+      <p class="text-center font-albert font-light text-lightModeText dark:text-darkModeText">Designed & Built by Joseph Castillo</p>
+      <a target="_blank" rel="noopener noreferrer" href="https://github.com/jcstlo/personal-site">{githubIcon}</a>
+    </div>
   )
 }
 
