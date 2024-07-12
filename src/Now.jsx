da15f08338a02ea0b9338f0ce8e8fcb0201a285b
jcstlo <joseph.cast0226@gmail.com>
2024-07-11 18:12:48 -0700

    Update now page

--- src/Now.jsx
+++ src/Now.jsx
@@ -8,14 +8,14 @@ function Now() {
         <h1 class="text-5xl pb-7 font-albert font-bold text-center sm:text-left ml-3 text-lightModeText dark:text-darkModeText">What I'm Doing Now</h1>
         <p class="font-albert font-normal ml-3 text-lightModeText dark:text-darkModeText">This is a now page, inspired by <a target="_blank" rel="noopener noreferrer" href="https://nownownow.com/about" class="underline">Derek Sivers.</a></p>
         <ul class="mt-10 ml-5 mr-3">
-          <li class="list-disc font-albert font-normal text-xl ml-3 text-lightModeText dark:text-darkModeText">Building my personal website (improving my front-end skills in the process)</li>
+          <li class="list-disc font-albert font-normal text-xl ml-3 text-lightModeText dark:text-darkModeText">Building a full-stack application with React and Django</li>
           <li class="list-disc font-albert font-normal text-xl ml-3 text-lightModeText dark:text-darkModeText">Getting a CS degree from WGU</li>
           <li class="list-disc font-albert font-normal text-xl ml-3 text-lightModeText dark:text-darkModeText">Playing ultimate frisbee</li>
           <li class="list-disc font-albert font-normal text-xl ml-3 text-lightModeText dark:text-darkModeText">Reading "Project Hail Mary" by Andy Weir</li>
           <li class="list-disc font-albert font-normal text-xl ml-3 text-lightModeText dark:text-darkModeText">Going on scenic hikes near Vancouver, BC</li>
         </ul>
       </div>
-      <p class="font-albert font-light mb-10 ml-3 text-lightModeText dark:text-darkModeText">Last updated: June 22, 2024</p>
+      <p class="font-albert font-light mb-10 ml-3 text-lightModeText dark:text-darkModeText">Last updated: July 11, 2024</p>
       <ContactMe></ContactMe>
       <Footer></Footer>
       </div>
